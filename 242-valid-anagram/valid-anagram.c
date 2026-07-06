3b9274b740d79b84e44d4a60ec19bea2009a4641
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-07-06 21:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (36.52%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdbool.h>
+
+bool isAnagram(char* s, char* t) {
+
+    int n = strlen(s);
+    int m = strlen(t);
+
+    if(n != m){
+        return false;
+    }
+
+    int count[26] = {0};
+
+    for(int i = 0; i < n; i++){
+        count[s[i] - 'a']++;
+        count[t[i] - 'a']--;
+    }
+
+    for(int i = 0; i < 26; i++){
+        if(count[i] != 0){
+            return false;
+        }
+    }
+
+    return true;
+}
