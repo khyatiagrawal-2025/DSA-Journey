692d988f73b228020a98ff939a537aef4889afe3
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-07-08 20:24:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (33.49%) - LeetSync

--- 136-single-number/single-number.c
+++ 136-single-number/single-number.c
@@ -1,21 +1,10 @@
 int singleNumber(int* nums, int numsSize) {
 
+    int ans = 0;
+
     for(int i = 0; i < numsSize; i++) {
-
-        int count = 0;
-
-        for(int j = 0; j < numsSize; j++) {
-
-            if(nums[i] == nums[j]) {
-                count++;
-            }
-
-        }
-
-        if(count == 1) {
-            return nums[i];
-        }
+        ans = ans ^ nums[i];
     }
 
-    return -1;
+    return ans;
 }
