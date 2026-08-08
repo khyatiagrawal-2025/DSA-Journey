9ec16e957d7891b5fa73fbd3fc0b214f8f117128
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-08-08 14:05:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (13.56%) - LeetSync

---
+++ LeetCode/605-can-place-flowers/can-place-flowers.c
@@ -0,0 +1,25 @@
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+
+    if(n == 0) {
+        return true;
+    }
+
+    for(int i = 0; i < flowerbedSize; i++) {
+
+        if(flowerbed[i] == 0) {
+
+            if((i == 0 || flowerbed[i - 1] == 0) &&
+               (i == flowerbedSize - 1 || flowerbed[i + 1] == 0)) {
+
+                flowerbed[i] = 1;
+                n--;
+
+                if(n == 0) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
