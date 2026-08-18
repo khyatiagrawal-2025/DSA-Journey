b5981a60c08b83e9d957307b070d5a02139789b3
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-08-18 13:40:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (22.28%) - LeetSync

--- LeetCode/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.c
+++ LeetCode/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.c
@@ -4,29 +4,27 @@ int* intersect(int* nums1, int nums1Size,
                int* nums2, int nums2Size,
                int* returnSize) {
 
-    int* ans = malloc(nums1Size * sizeof(int));
-    int* used = calloc(nums2Size, sizeof(int));
+    int count[1001] = {0};
 
-    int count = 0;
+    int* ans = malloc(nums1Size * sizeof(int));
+    int k = 0;
 
     for(int i = 0; i < nums1Size; i++) {
+        count[nums1[i]]++;
+    }
 
-        for(int j = 0; j < nums2Size; j++) {
+    for(int i = 0; i < nums2Size; i++) {
 
-            if(nums1[i] == nums2[j] && used[j] == 0) {
+        if(count[nums2[i]] > 0) {
 
-                ans[count] = nums1[i];
-                count++;
+            ans[k] = nums2[i];
+            k++;
 
-                used[j] = 1;
-                break;
-            }
+            count[nums2[i]]--;
         }
     }
 
-    *returnSize = count;
-
-    free(used);
+    *returnSize = k;
 
     return ans;
 }
