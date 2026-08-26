61adcbdc3142d65f9a16748353c631a83e066498
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-08-26 14:27:18 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (10.86%) - LeetSync

---
+++ LeetCode/11-container-with-most-water/container-with-most-water.c
@@ -0,0 +1,26 @@
+int maxArea(int* height, int heightSize) {
+
+    int left = 0;
+    int right = heightSize - 1;
+    int max = 0;
+
+    while (left < right) {
+
+        int area;
+
+        if (height[left] < height[right]) {
+            area = height[left] * (right - left);
+            left++;
+        }
+        else {
+            area = height[right] * (right - left);
+            right--;
+        }
+
+        if (area > max) {
+            max = area;
+        }
+    }
+
+    return max;
+}
