e1d57b19d27d667fbf46686d478440e6d99a1f35
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-08-30 20:36:12 +0530

    Time: 1 ms (98.68%) | Memory: 42.3 MB (90.39%) - LeetSync

--- LeetCode/69-sqrtx/sqrtx.java
+++ LeetCode/69-sqrtx/sqrtx.java
@@ -1,11 +1,27 @@
 class Solution {
     public int mySqrt(int x) {
-        int i = 0;
 
-        while ((long)i * i <= x) {
-            i++;
+        if(x < 2) {
+            return x;
         }
 
-        return i - 1;
+        int left = 1;
+        int right = x;
+        int ans = 0;
+
+        while(left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(mid <= x / mid) {
+                ans = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
     }
 }
