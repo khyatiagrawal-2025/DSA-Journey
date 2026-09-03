852c8421db3fbd999171499adf5ff1850dfe0538
Khyati Agrawal <khyatiagrawal9494@gmail.com>
2026-09-03 19:53:35 +0530

    Time: 98 ms (82.73%) | Memory: 9.5 MB (47.15%) - LeetSync

--- LeetCode/1-two-sum/two-sum.c
+++ LeetCode/1-two-sum/two-sum.c
@@ -3,6 +3,7 @@
  */
 #include <stdlib.h>
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+
     for (int i=0; i<numsSize; i++){
         for (int j=i+1; j<numsSize; j++){
             if((nums[i]+nums[j])==target){
@@ -14,5 +15,6 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
             }
         }
     }
+
     return NULL;
 }
